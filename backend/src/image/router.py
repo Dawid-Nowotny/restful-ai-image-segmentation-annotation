10fa916ce1f8acf010a920a889a925a461dd2130
Arkadiusz Wolski <arkadiusz.wolski5551@gmail.com>
2024-04-20 12:48:45 +0200

    refactor: image router accepting image data and image file

--- backend/src/image/router.py
+++ backend/src/image/router.py
@@ -1,12 +1,19 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, UploadFile, File, Body, Form, Depends
+from typing import Annotated
 from .services import UserServices
+from .schemas import ImageData
 
 router = APIRouter()
 
 @router.post("/upload")
-async def upload():
+async def upload(imageData: ImageData = Depends(), file: UploadFile = File(...)):
+  #print(ImageSchema)
+  #print(image.filename)
   userServices = UserServices()
   userServices.updateImage()
-  return {"message": "Image uploaded"}
+  return {
+    "JSON Payload": {"name": "bebe"},
+    "Filename": file.filename
+  }
 
   
