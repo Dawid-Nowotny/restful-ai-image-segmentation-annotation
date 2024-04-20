f064af00d975424964bbe2f607cc1f0fca92bc24
Arkadiusz Wolski <arkadiusz.wolski5551@gmail.com>
2024-04-20 15:14:16 +0200

    fix: removed async from images/upload endpoint

--- backend/src/main.py
+++ backend/src/main.py
@@ -4,7 +4,3 @@ from image import router as image_router
 app = FastAPI()
 
 app.include_router(image_router.router, prefix="/images", tags=["image"])
-
-@app.get("/")
-async def root():
-    return {"message": "Hello` World"}
