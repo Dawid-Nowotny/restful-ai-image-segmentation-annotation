31a9f6fc9c57c55241f0ad7fba4c738a742a9eb3
Arkadiusz Wolski <arkadiusz.wolski5551@gmail.com>
2024-04-20 13:33:15 +0200

    feat: uploaded file validation

--- backend/src/image/services.py
+++ backend/src/image/services.py
@@ -1,9 +1,11 @@
-from fastapi import UploadFile, File
+from fastapi import UploadFile, File, HTTPException, status
+from typing import IO
 from sqlalchemy import insert
 from datetime import date
 from models import Image
 from database import engine
 from .schemas import ImageData
+import filetype
 
 class UserServices:
   def add_image_to_database(self, image_data: ImageData, image: UploadFile = File(...)):
@@ -23,3 +25,31 @@ class UserServices:
     with engine.connect() as conn:
       conn.execute(stmt)
       conn.commit()
+
+  def validate_file_size_type(self, file: IO):
+    FILE_SIZE = 2097152 # 5MB
+    accepted_file_types = ["image/png", "image/jpeg", "image/jpg", "png", "jpeg", "jpg"] 
+
+    file_info = filetype.guess(file.file)
+    if file_info is None:
+        raise HTTPException(
+            status_code=status.HTTP_415_UNSUPPORTED_MEDIA_TYPE,
+            detail="Unable to determine file type",
+        )
+
+    detected_content_type = file_info.extension.lower()
+
+    if (
+        file.content_type not in accepted_file_types
+        or detected_content_type not in accepted_file_types
+    ):
+        raise HTTPException(
+            status_code=status.HTTP_415_UNSUPPORTED_MEDIA_TYPE,
+            detail="Unsupported file type",
+        )
+
+    real_file_size = 0
+    for chunk in file.file:
+        real_file_size += len(chunk)
+        if real_file_size > FILE_SIZE:
+            raise HTTPException(status_code=status.HTTP_413_REQUEST_ENTITY_TOO_LARGE, detail="Too large")
