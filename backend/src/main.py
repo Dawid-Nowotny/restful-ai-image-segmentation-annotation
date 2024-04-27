538a46746e043e977955051522459c1b06ea64a0
Arkadiusz Wolski <arkadiusz.wolski5551@gmail.com>
2024-04-27 21:06:10 +0200

    feat: CORS allowed origin and methods

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,6 +1,20 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from image import router as image_router
 
 app = FastAPI()
+
+origins = [
+    "http://localhost:4200",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_methods=["*"],
+    # allow_headers=["*"],
+)
+
+
 
 app.include_router(image_router.router, prefix="/images", tags=["image"])
