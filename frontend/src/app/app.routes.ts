6cb2974a1a08d8eba781012bd4dd78115d0f238f
Bartlomiej Palus <bartlomiej.palus@poczta.onet.pl>
2024-04-29 16:29:38 +0200

    Added image-view route

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -3,11 +3,13 @@ import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { MainPageComponent } from './main-page/main-page.component';
 import { NotFoundComponent } from './not-found/not-found.component';
+import { ImageViewComponent } from './image-view/image-view.component';
 
 export const routes: Routes = [
     { path: '', component: MainPageComponent },
     { path: 'login', component: LoginComponent },
     { path: 'register', component: RegisterComponent },
+    { path: 'image-view/:id', component: ImageViewComponent },
     { path: '**', pathMatch: 'full', 
         component: NotFoundComponent },
   ];
