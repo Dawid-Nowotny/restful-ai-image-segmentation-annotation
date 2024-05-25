7972bac7daae2c934e8156c65c513bb011b54f0d
Ardon <radekmedalion@gmail.com>
2024-05-25 16:25:41 +0200

    Added profile info request, user panel template modification

--- frontend/src/app/user-panel/user-panel.component.ts
+++ frontend/src/app/user-panel/user-panel.component.ts
@@ -1,17 +1,38 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ServerService } from '../services/server.service';
+import { CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'app-user-panel',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './user-panel.component.html',
   styleUrl: './user-panel.component.css'
 })
+
 export class UserPanelComponent {
 
-  username = "Ardon";
-  role = "użytkownik";
-  opis = "jakiś opis";
-  image_count = "33";
+  currentComponent: string | null = null;
+  username: string = "";
+  role: string = "";
+  image_count: number = 0;
+  isAuthorizationEnabled: boolean = false;
+  authorizationButtonMessage = (this.isAuthorizationEnabled)? "Deaktywuj 2FA" : "Aktywuj 2FA";
+  isLoggedUser: boolean = false;
+
+  constructor(private route: ActivatedRoute, private serverService: ServerService) { }
+
+  ngOnInit(): void {
+    this.serverService.getProfileInfo("radek").subscribe(data => {
+      this.username = data.username;
+      this.role = data.role;
+      this.image_count = data.images_count;
+    })
+
+  }
+
+
 
 }
