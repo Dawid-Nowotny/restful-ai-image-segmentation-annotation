af3056c520314d783ac6e3c1a4bbf07cae95abf6
Arkadiusz Wolski <arkadiusz.wolski5551@gmail.com>
2024-05-31 18:58:29 +0200

    feat: ChartService

--- frontend/src/app/statistics-panel/statistics-panel.component.ts
+++ frontend/src/app/statistics-panel/statistics-panel.component.ts
@@ -5,6 +5,7 @@ import { TabDirective, TabsModule } from 'ngx-bootstrap/tabs';
 import { ServerService } from '../services/server.service';
 import { LoggedUserService } from '../services/logged-user.service';
 import { CommonModule } from '@angular/common';
+import { ChartService } from '../services/chart.service';
 
 type TopTagData = {
 	tag: string,
@@ -68,176 +69,34 @@ export class StatisticsPanelComponent implements AfterViewInit {
 
 	chart!: Chart;
 
-	constructor(private serverService: ServerService, public loggedUserService: LoggedUserService) {
+	constructor(
+		public loggedUserService: LoggedUserService,
+		private chartService: ChartService
+	) {
 		Chart.register(ChartDataLabels);
 	}
 
 	ngAfterViewInit(): void {
-		this.createTopTagsChart();
+		this.chartService.createTopTagsChart(this.topTagsChartCanvas);
 	}
 
 	handleTabChange(tab: TabDirective) {
 		switch (tab.id) {
-			case 'tab1': this.createTopTagsChart(); break;
-			case 'tab2': this.createPopularTagsByMonthChart(); break;
-			case 'tab3': this.createTopClassesChart(); break;
-			case 'tab4': this.createPopularClassesByMonthChart(); break;
-			case 'tab5': this.createTopUploadersChart(); break;
-			case 'tab6': this.createTopCommentersChart(); break;
-			case 'tab7': this.createTopModeratorsChart(); break;
+			case 'tab1':
+				this.chartService.createTopTagsChart(this.topTagsChartCanvas); break;
+			case 'tab2':
+				this.chartService.createPopularTagsByMonthChart(this.popularTagsByMonthChartCanvas); break;
+			case 'tab3':
+				this.chartService.createTopClassesChart(this.topClassesChartCanvas); break;
+			case 'tab4':
+				this.chartService.createPopularClassesByMonthChart(this.popularClassesByMonthChartCanvas); break;
+			case 'tab5':
+				this.chartService.createTopUploadersChart(this.topUploadersChartCanvas); break;
+			case 'tab6':
+				this.chartService.createTopCommentersChart(this.topCommentersChartCanvas); break;
+			case 'tab7':
+				this.chartService.createTopModeratorsChart(this.topModeratorsChartCanvas); break;
 		}
-	}
-
-	createTopTagsChart() {
-		this.serverService.getTopTags(10).subscribe({
-			next: (response: any) => {
-				let labels = response.map((tag: any) => tag.tag);
-				let data = response.map((tag: any) => tag.count);
-				this.createChart(this.topTagsChartCanvas, "Top 10 tagów", labels, data);
-			},
-			error: (error: Error) => {
-				console.log(error);
-			}
-		})
-	}
-
-	createPopularTagsByMonthChart() {
-		this.serverService.getPopularTagsByMonth().subscribe({
-			next: (response: any) => {
-				let labels = response.map((tag: any) => [tag.month, `tag: ${tag.top_tag.tag}`]);
-				let data = response.map((tag: any) => tag.top_tag.count);
-				this.createChart(this.popularTagsByMonthChartCanvas, "Popularne tagi w poszczególnych miesiącach", labels, data);
-			},
-			error: (error: Error) => {
-				console.log(error);
-			}
-		})
-	}
-
-	createTopClassesChart() {
-		this.serverService.getTopClasses(10).subscribe({
-			next: (response: any) => {
-				let labels = response.map((imageClass: TopClassesData) => imageClass.class_name);
-				let data = response.map((imageClass: TopClassesData) => imageClass.count);
-				this.createChart(this.topClassesChartCanvas, "Top 10 klas", labels, data);
-			},
-			error: (error: Error) => {
-				console.log(error);
-			}
-		})
-	}
-
-	createPopularClassesByMonthChart() {
-		this.serverService.getPopularClassesByMonth().subscribe({
-			next: (response: PopularClassesByMonthData[]) => {
-				let labels: any = response.map(imageClass => [imageClass.month, `klasa: ${imageClass.top_classes.class_name}`]);
-				let data = response.map((imageClass: PopularClassesByMonthData) => imageClass.top_classes.count);
-				this.createChart(
-					this.popularClassesByMonthChartCanvas,
-					"Popularne klasy w poszczególnych miesiącach",
-					labels,
-					data
-				);
-			},
-			error: (error: Error) => {
-				console.log(error);
-			}
-		})
-	}
-
-	createTopUploadersChart() {
-		this.serverService.getTopUploaders(this.loggedUserService.getAccessToken(), 10).subscribe({
-			next: (response: TopUploadersData[]) => {
-				let labels = response.map((uploader: TopUploadersData) => uploader.username);
-				let data = response.map((uploader: TopUploadersData) => uploader.upload_count);
-				this.createChart(this.topUploadersChartCanvas, "Top 10 uploaderów", labels, data);
-			},
-			error: (error: Error) => {
-				console.log(error);
-			}
-
-		})
-	}
-
-	createTopCommentersChart() {
-		this.serverService.getTopCommenters(this.loggedUserService.getAccessToken(), 10).subscribe({
-			next: (response: TopCommentersData[]) => {
-				let labels = response.map((uploader: TopCommentersData) => uploader.username);
-				let data = response.map((uploader: TopCommentersData) => uploader.comment_count);
-				this.createChart(this.topCommentersChartCanvas, "Top 10 komentujących", labels, data);
-			},
-			error: (error: Error) => {
-				console.log(error);
-			}
-		})
-	}
-
-	createTopModeratorsChart() {
-		this.serverService.getTopModerators(this.loggedUserService.getAccessToken(), 10).subscribe({
-			next: (response: TopModeratorsData[]) => {
-				let labels = response.map((moderator: TopModeratorsData) => moderator.username);
-				let data = response.map((moderator: TopModeratorsData) => moderator.moderated_count);
-				this.createChart(this.topModeratorsChartCanvas, "Top 10 moderatorów", labels, data);
-			},
-			error: (error: Error) => {
-				console.log(error);
-			}
-		})
-	}
-
-	createChart(
-		chartElementRef: ElementRef<HTMLCanvasElement>,
-		title: string,
-		labels: string[],
-		data: number[],
-	) {
-		if (this.chart) {
-			this.chart.destroy();
-		}
-
-		this.chart = new Chart(chartElementRef.nativeElement, {
-			type: 'bar',
-			data: {
-				labels: labels,
-				datasets: [
-					{
-						label: title,
-						data: data,
-						backgroundColor: 'rgba(46, 204, 113, 0.2)',
-						borderColor: 'rgba(46, 204, 113, 1)',
-						borderWidth: 2,
-					}
-				]
-			},
-			options: {
-				maintainAspectRatio: false,
-				scales: {
-					x: {
-						ticks: {
-							font: {
-								size: 16
-							}
-						},
-					},
-					y: {
-						ticks: {
-							font: {
-								size: 16
-							}
-						}
-					}
-				},
-				plugins: {
-					legend: {
-						labels: {
-							font: {
-								size: 20
-							},
-						},
-					},
-				}
-			}
-		});
 	}
 
 }
