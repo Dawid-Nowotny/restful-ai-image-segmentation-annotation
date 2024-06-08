25cdf1bde02ec696d0e1a8a7d9fc02336d872d13
Arkadiusz Wolski <arkadiusz.wolski5551@gmail.com>
2024-06-08 12:29:08 +0200

    update: app config

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,9 +1,13 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
+import { HttpClientModule, provideHttpClient, withInterceptors } from '@angular/common/http';
+import { universalInterceptor } from './universal.interceptor';
 
 export const appConfig: ApplicationConfig = {
     providers: [
-        provideRouter(routes)
+        importProvidersFrom(HttpClientModule),
+        provideRouter(routes),
+        provideHttpClient(withInterceptors([universalInterceptor]))
     ]
 };
--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,19 +1,10 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
-import { HttpClientModule, provideHttpClient, withInterceptors } from "@angular/common/http";
-import { enableProdMode, importProvidersFrom } from "@angular/core";
-import { provideRouter } from '@angular/router';
-import { routes } from './app/app.routes';
-import { universalInterceptor } from './app/universal.interceptor';
+import { appConfig } from './app/app.config';
 
 
 bootstrapApplication(AppComponent, {
-    providers: [
-        importProvidersFrom(HttpClientModule),
-        provideRouter(routes),
-        provideHttpClient(withInterceptors([universalInterceptor]))
-    ],
+    providers: appConfig.providers
     
 })
     .catch((err) => console.error(err));
